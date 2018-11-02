101c2dc1715936b0ec62ecbba4a8bc8ab998e5e7
anji <anji.naga1@gmail.com>
2018-11-03 00:32:20 +0530

    login form design

---
+++ anji/login.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+	<link rel="stylesheet" type="text/css" href="assets\css\style.css">
+		<link rel="stylesheet" type="text/css" href="http://localhost//UI-PRACTICE/anji/assets/images/back.jpg">
+</head>
+<body>
+	<div class="img">
+<img src="http://localhost//UI-PRACTICE/anji/assets/images/user.png" alt="avatar" height="100px" width="100px"> 
+</div>
+
+<div class="loginbox">
+	<h2>Login Here</h2>
+	<form action="#" method="post">
+	<div class="fields">
+		<p>User name</p>
+		<input type="text" value="" name="username" placeholder="Enter username" required="">
+		<p>Password</p>
+		<input type="Password" value="" name="Password" placeholder="**********" required="">
+		<input type="submit" value="Login">
+	</div>
+	</form>
+</div>
+</body>
+</html>
